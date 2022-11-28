9c28d3ea9354a838699ffceac6c39fa6f0d97b82
Mytych <105545597+Mytych@users.noreply.github.com>
2022-11-28 19:55:54 +0100

    zedytowane v2

--- login.js
+++ login.js
@@ -2,6 +2,24 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit"); //logowanie przez przycisk
 const loginErrorMsg = document.getElementById("login-error-msg"); //wiadomosc 
 
+const checkbox = document.querySelector("#showPassword");
+checkbox.addEventListener("click", togglePassword);
+
+function togglePassword () {
+ const password = document.querySelector("#password-field");
+  
+    // jezeli checkbox jest zaznaczony
+    if(checkbox.checked) {
+        console.log("Checkbox jest zaznaczony. JavaScript, pokaz haslo!");
+        password.setAttribute("type", "text");
+        }
+    // jezeli checkbox nie jest zaznaczony
+    else {
+        console.log("Checkbox odznaczony. Ukryj haslo!");
+        password.setAttribute("type", "password");
+        }
+}
+
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
